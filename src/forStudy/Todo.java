dd56527c25309e9d89fffd9ba30ed131221bd1e4
sonyebin <sonyeabin38@naver.com>
2023-08-07 00:00:00 +0900

    LocalDate 사용... finish 메소드 수정 전

---
+++ src/forStudy/Todo.java
@@ -0,0 +1,61 @@
+package forStudy;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Todo {
+    Scanner s = new Scanner(System.in);
+    public static ArrayList<Todo> todos = new ArrayList<>();
+    String todo;
+    int day;
+    public static int len = 0;
+    LocalDate limitdate;
+
+    public Todo() {
+    }
+
+    public Todo(String todo, LocalDate limitdate) {
+        this.todo = todo;
+        this.limitdate = limitdate;
+    }
+
+
+    public void addSchedule() {
+        System.out.print("할 일을 입력하시오: ");
+        todo = s.nextLine();
+        System.out.print("마감일을 입력하시오: ");
+        day = s.nextInt();
+        this.limitdate = LocalDate.of(2023, 8, day);
+        todos.add(new Todo(todo,limitdate));
+        len = 1;
+    }
+
+    public void changeSchedule() {
+        System.out.print("수정할 일을 입력하시오: ");
+        String work = s.nextLine();
+        for(Todo newdata : todos){
+            if (work.equals(newdata.todo)) {
+                System.out.print("할 일을 다시 입력하세요: ");
+                newdata.todo = s.nextLine();
+                System.out.print("마감일을 다시 입력하세요: ");
+                int newday = s.nextInt();
+                newdata.limitdate = LocalDate.of(2023,8,newday);
+            }
+        }
+    }
+
+    public void finishSchedule() {
+        System.out.print("완료한 일을 입력하시오: ");
+        String fin = s.nextLine();
+        for (Todo finwork : todos){
+            if(fin.equals(finwork.todo)){
+                finwork.todo = fin+"-완료됨"; }
+            }
+        }
+
+    @Override
+    public String toString(){
+        return todo+" / 마감일: "+limitdate;
+    }
+}
